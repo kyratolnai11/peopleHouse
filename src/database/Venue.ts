9e5c4f7959434ef44392b60951ef9520eb026b82
kyra <kyra.tolnai@lego.com>
2023-10-16 08:30:25 +0200

    I am able to run on mine

--- src/database/Venue.ts
+++ src/database/Venue.ts
@@ -1,34 +1,37 @@
 import { listVenues } from "../graphql/queries";
 import { createVenue } from "../graphql/mutations";
-import { API, graphqlOperation } from 'aws-amplify';
+import { API, graphqlOperation } from "aws-amplify";
 import { CreateVenueInput, ModelVenueConnection } from "../API";
 
-export async function fetchVenues() : Promise<ModelVenueConnection | undefined >{
+export async function fetchVenues(): Promise<ModelVenueConnection | undefined> {
   try {
     console.log("Getting venues...");
-    var venueData: any = await API.graphql(graphqlOperation(listVenues));
-    let venues: ModelVenueConnection = venueData.data.listVenues.items;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const venueData: any = await API.graphql(graphqlOperation(listVenues));
+    const venues: ModelVenueConnection = venueData.data.listVenues.items;
     console.log("In the method:", venues);
     return venues;
   } catch (err) {
-    console.log('error fetching todos:', err);
+    console.log("Error fetching venues:", err);
   }
 }
 
 export const addVenue = async () => {
   try {
     console.log("Try to add venue...");
-    let venueToAdd: CreateVenueInput = {
+    const venueToAdd: CreateVenueInput = {
       id: "1",
       name: "Test data",
       shortDescription: "Test data",
       description: "Test data",
       capacity: 50,
-      openingHours: "00:00 - 24:00"
+      openingHours: "00:00 - 24:00",
     };
-    var ras = await API.graphql(graphqlOperation(createVenue, {input: venueToAdd}));
-    console.log(ras)
+    const ras = await API.graphql(
+      graphqlOperation(createVenue, { input: venueToAdd })
+    );
+    console.log(ras);
   } catch (err) {
-    console.log('error creating venue:', err);
+    console.log("error creating venue:", err);
   }
-}
+};
--- src/screens/Venues.tsx
+++ src/screens/Venues.tsx
@@ -1,48 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import VenueCard from '../components/VenueCard';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { fetchVenues } from "../database/Venue";
-import { Venue, ModelVenueConnection } from "../API";
+import { ModelVenueConnection } from "../API";
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "flex-start",
-      justifyContent: "center",
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        paddingBottom: 0,
-        padding: 10,
-      },
-      description: {
-        fontSize: 14,
-        textAlign: 'left',
-        paddingBottom: 0,
-        padding: 10,
-      },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingBottom: 0,
+    padding: 10,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: "left",
+    paddingBottom: 0,
+    padding: 10,
+  },
+});
 
 const Venues = () => {
-
   const [venues, setVenues] = useState<ModelVenueConnection>();
-  
+
   useEffect(() => {
     fetchVenues()
-    .then((venuesdata) => {
-      setVenues(venuesdata);
-      console.log(venues);
-    })
-    .catch((error) => {
-      console.error("Error fetching venues:", error);
-    });
+      .then((venuesdata) => {
+        setVenues(venuesdata);
+        console.log(venues);
+      })
+      .catch((error) => {
+        console.error("Error fetching venues:", error);
+      });
   }, []);
 
-  const title = 'We provide the place – you bring the fun!';
-  const description = 'Here at the LEGO People House you have your pick of fun venues to visit. Get crafty in the Creative Studio, hang with your Crew in the Heart, feel the burn in the LEGO Gym, or have a cozy time in the Fireplace Lounge. Come, explore, and enjoy. We can’t wait to see you in People House.'
+  const title = "We provide the place – you bring the fun!";
+  const description =
+    "Here at the LEGO People House you have your pick of fun venues to visit. Get crafty in the Creative Studio, hang with your Crew in the Heart, feel the burn in the LEGO Gym, or have a cozy time in the Fireplace Lounge. Come, explore, and enjoy. We can’t wait to see you in People House.";
 
   // if(venues)
   // {
@@ -51,7 +50,7 @@ const Venues = () => {
   //     <VenueCard props={i} />}
   //   })}
 
-  return ( 
+  return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.description}>{description}</Text>
