f4026aff5f599af36602c2f141dc1fd8b3d44022
kyra <kyra.tolnai@lego.com>
2023-10-16 11:20:10 +0200

    Adding venues to navigation

--- App.tsx
+++ App.tsx
@@ -4,27 +4,10 @@ import awsconfig from "./src/aws-exports.js";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import DrawerNavigation from "./src/components/navigation/DrawerNavigation.tsx";
-import { addVenue, fetchVenues } from "./src/database/Venue.ts";
-import Venues from "./src/screens/Venues.tsx";
-
-//Important: Remember to import Auth when we get to the authentication part
 
 Amplify.configure(awsconfig);
 
 export default function App() {
-  // addVenue()
-  //   .then((result) => {
-  //     console.log(result);
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error:", error);
-  //   });
-  //fetchVenues();
-  return (
-    <Venues />
-  );
-
-
   return (
     <NavigationContainer>
       <DrawerNavigation />
