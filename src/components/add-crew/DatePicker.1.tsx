00d31406149e5f8ca76806b77c1bdc293563e6e2
kyra <kyra.tolnai@lego.com>
2023-10-23 09:53:25 +0200

    new datepikcer

---
+++ src/components/add-crew/DatePicker.1.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Button, View } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+
+const DatePicker: React.FC = () => {
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode("date");
+  };
+
+  const showTimepicker = () => {
+    showMode("time");
+  };
+
+  return (
+    <View>
+      <Button onPress={showDatepicker} title="Show date picker!" />
+      <Button onPress={showTimepicker} title="Show time picker!" />
+      <Text>selected: {date.toLocaleString()}</Text>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode="date"
+          is24Hour={true}
+          onChange={onChange}
+        />
+      )}
+    </View>
+  );
+};
