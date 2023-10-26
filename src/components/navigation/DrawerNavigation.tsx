1e2b7c03b5cdda48a1dfc6e63c6e6625afc41a81
kyra <kyra.tolnai@lego.com>
2023-10-26 21:08:36 +0200

    Correcting small mistake

--- src/components/navigation/DrawerNavigation.tsx
+++ src/components/navigation/DrawerNavigation.tsx
@@ -16,7 +16,7 @@ const DrawerNavigation: React.FC = () => {
 
   return (
     <Drawer.Navigator
-      initialRouteName="Add Crew"
+      initialRouteName="Home"
       drawerContent={CustomDrawer}
       screenOptions={{
         headerStyle: { backgroundColor: Colors.dark.primary, height: 120 },
