04ffd224a8c42b1ebeec707c2fc854f73e34747d
kyra <kyra.tolnai@lego.com>
2023-11-05 12:42:41 +0100

    Extra fields

---
+++ src/components/create-event/CreateEventStyles.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+import Colors from "../../../utils/theme";
+
+export const createEventStyles = StyleSheet.create({
+  ticketsHeader: {
+    backgroundColor: Colors.light.primary,
+    transform: "rotate(-1deg)",
+    width: 180,
+    height: 50,
+    justifyContent: "center",
+    position: "absolute",
+    marginTop: -20,
+  },
+  ticketHeaderText: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  ticketContainer: {
+    borderWidth: 1,
+    borderColor: Colors.light.textPrimary,
+    backgroundColor: Colors.light.white,
+    width: 350,
+    height: 255,
+    borderRadius: 20,
+    position: "relative",
+    alignItems: "center",
+    marginTop: 30,
+  },
+  ticketText: {
+    marginTop: 50,
+    fontSize: 20,
+  },
+  chairImage: {
+    width: 100,
+    resizeMode: "cover",
+  },
+});
--- src/screens/CreateEventScreen.tsx
+++ src/screens/CreateEventScreen.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { View, ScrollView, SafeAreaView, Text, Alert } from "react-native";
+import {
+  View,
+  ScrollView,
+  SafeAreaView,
+  Text,
+  Alert,
+  TextInput,
+  Image,
+} from "react-native";
 import { sharedStyles } from "../../utils/SharedStyles";
 import Colors from "../../utils/theme";
-import { useForm } from "react-hook-form";
-import { addEvent } from "../database/EventDBConnection";
+import { Controller, useForm } from "react-hook-form";
+import CustomButton from "../components/event-screen/CustomButton";
+import { createEventStyles } from "../components/create-event/CreateEventStyles";
 
 export type CreateEventForm = {
   eventTitle: string;
@@ -18,23 +27,20 @@ export type CreateEventForm = {
 };
 
 const CreateEventScreen: React.FC = () => {
-  const { handleSubmit, control, setValue, reset } = useForm<CreateEventForm>();
+  const { handleSubmit, control, reset } = useForm<CreateEventForm>();
 
   const onSubmit = async (data: CreateEventForm) => {
-    await addEvent(data);
+    console.log(data);
+    // await addEvent(data);
     reset();
-    Alert.alert(
-      "Event added",
-      "You have successfully added a new event!",
-      [
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel",
-        },
-        { text: "OK", onPress: () => console.log("OK Pressed") },
-      ]
-    );
+    Alert.alert("Event added", "You have successfully added a new event!", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
     console.log("Form submitted");
   };
   return (
@@ -45,8 +51,120 @@ const CreateEventScreen: React.FC = () => {
       }}
     >
       <ScrollView style={{ width: "100%" }}>
-        <View style={sharedStyles.mainContainer}>
-          <Text style={sharedStyles.screenTitle}>Create event</Text>
+        <Text style={sharedStyles.screenTitle}>Create event</Text>
+        <View
+          style={{
+            width: "100%",
+            padding: 20,
+            backgroundColor: Colors.light.secondary,
+          }}
+        >
+          <Text
+            style={[sharedStyles.text, { color: Colors.light.textSecondary }]}
+          >
+            Event title
+          </Text>
+          <Controller
+            name="eventTitle"
+            control={control}
+            render={({ field }) => (
+              <TextInput
+                placeholder="Enter title"
+                value={field.value}
+                onChangeText={field.onChange}
+                style={[
+                  sharedStyles.input,
+                  { backgroundColor: Colors.light.textSecondary },
+                ]}
+              />
+            )}
+          />
+          <Text
+            style={[sharedStyles.text, { color: Colors.light.textSecondary }]}
+          >
+            Brief
+          </Text>
+          <Controller
+            name="brief"
+            control={control}
+            render={({ field }) => (
+              <TextInput
+                value={field.value}
+                onChangeText={field.onChange}
+                numberOfLines={4}
+                multiline={true}
+                style={[
+                  sharedStyles.input,
+                  { backgroundColor: Colors.light.textSecondary, height: 100 },
+                ]}
+              />
+            )}
+          />
+
+          <View style={createEventStyles.ticketContainer}>
+            <View style={createEventStyles.ticketsHeader}>
+              <Text style={createEventStyles.ticketHeaderText}>Tickets</Text>
+            </View>
+            <Text style={[sharedStyles.text, { marginTop: 60 }]}>
+              Number of tickets
+            </Text>
+            <Controller
+              name="nuOfTickets"
+              control={control}
+              render={({ field }) => (
+                <TextInput
+                  placeholder="1"
+                  returnKeyType="done"
+                  value={field.value ? field.value.toString() : ""}
+                  keyboardType="numeric"
+                  onChangeText={field.onChange}
+                  style={[sharedStyles.input, { width: 150 }]}
+                />
+              )}
+            />
+            <Image
+              source={require("../../assets/event-screen/chair.png")}
+              style={createEventStyles.chairImage}
+            />
+          </View>
+        </View>
+        <View style={{ padding: 20 }}>
+          <Text style={[sharedStyles.text]}>Description</Text>
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => (
+              <TextInput
+                value={field.value}
+                onChangeText={field.onChange}
+                numberOfLines={70}
+                multiline={true}
+                style={[
+                  sharedStyles.input,
+                  { backgroundColor: Colors.light.textSecondary, height: 200 },
+                ]}
+              />
+            )}
+          />
+          <Text style={[sharedStyles.text]}>Agenda (optional)</Text>
+          <Controller
+            name="agenda"
+            control={control}
+            render={({ field }) => (
+              <TextInput
+                value={field.value}
+                onChangeText={field.onChange}
+                numberOfLines={70}
+                multiline={true}
+                style={[
+                  sharedStyles.input,
+                  { backgroundColor: Colors.light.textSecondary, height: 200 },
+                ]}
+              />
+            )}
+          />
+
+          <CustomButton action={handleSubmit(onSubmit)} name="Add event" />
         </View>
       </ScrollView>
     </SafeAreaView>
