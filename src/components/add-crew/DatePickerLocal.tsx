118b82d38ac5a2f1c51cdb1c6a8c758dd1d321c3
kyra <kyra.tolnai@lego.com>
2023-11-06 12:02:11 +0100

    Simplified date picker

--- src/components/add-crew/DatePickerLocal.tsx
+++ src/components/add-crew/DatePickerLocal.tsx
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import { UseFormSetValue } from "react-hook-form";
 import { AddCrewForm } from "./CrewDropDown";
-import { sharedStyles } from "../../../utils/SharedStyles";
-import { addCrewStyles } from "./StyleSheet";
 
 type DatePickerProps = {
   setValueCrew?: UseFormSetValue<AddCrewForm>;
@@ -21,11 +19,10 @@ const DatePicker: React.FC<DatePickerProps> = ({
   setEventEndTime,
   setEventStartDate,
   setEventStartTime,
-  name,
+
   type,
 }) => {
   const [date, setDate] = useState(new Date());
-  const [show, setShow] = useState(false);
 
   const onChange = (event: any, selectedDate: any) => {
     const currentDate = selectedDate;
@@ -37,37 +34,17 @@ const DatePicker: React.FC<DatePickerProps> = ({
     setEventStartDate && setEventStartDate(date);
     setEventStartTime && setEventStartTime(date);
     setEventEndTime && setEventEndTime(date);
-
-    setShow(false);
-  };
-
-  const showDatepicker = () => {
-    setShow(true);
   };
 
   return (
-    <View>
-      <TouchableOpacity
-        onPress={showDatepicker}
-        style={addCrewStyles.dateButton}
-      >
-        <Text style={addCrewStyles.dateButtonText}>{name}</Text>
-      </TouchableOpacity>
-
-      {show && (
-        <View style={{ alignItems: "flex-start" }}>
-          <DateTimePicker
-            testID="dateTimePicker"
-            value={date}
-            mode={type === "Time" ? "time" : "date"}
-            is24Hour={true}
-            onChange={onChange}
-          />
-        </View>
-      )}
-      {date && (
-        <Text style={sharedStyles.text}>Selected: {date.toISOString()}</Text>
-      )}
+    <View style={{ alignItems: "flex-start" }}>
+      <DateTimePicker
+        testID="dateTimePicker"
+        value={date}
+        mode={type === "Time" ? "time" : "date"}
+        is24Hour={true}
+        onChange={onChange}
+      />
     </View>
   );
 };
