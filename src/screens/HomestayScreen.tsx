59a76424350e00e41b936aa93044c47eabae9b52
kyra <kyra.tolnai@lego.com>
2023-11-10 13:50:49 +0100

    Adding homestay to drawer navigation

---
+++ src/screens/HomestayScreen.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
+const HomestayScreen: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Hello</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomestayScreen;
