42933a9e3363062765c99fc97a9ab66b089a81ce
tolnaimicu <tolnai.milan22@gmail.com>
2023-11-24 11:52:53 +0100

    i doenst work

--- src/components/event-screen/SignUpForEventScreen.tsx
+++ src/components/event-screen/SignUpForEventScreen.tsx
@@ -64,7 +64,7 @@ const SignUpForEventScreen: React.FC<EventScreenProps> = ({ route }) => {
   const isFocused = useIsFocused();
   const [isUserRegistered, setIsUserRegistered] = useState<boolean>(false);
   const [isCrewRegistered, setIsCrewRegistered] = useState<{
-[key: string]: boolean;
+    [key: string]: boolean;
   }>({});
 
   const [isLoading, setIsLoading] = useState(true);
@@ -72,84 +72,79 @@ const SignUpForEventScreen: React.FC<EventScreenProps> = ({ route }) => {
 
 
   const fetchUserInfo = async () => {
-    if (isFocused) {
+    
       try {
         console.log("Start fetching user info...");
-  
-        const user = await Auth.currentAuthenticatedUser({
+
+        Auth.currentAuthenticatedUser({
           bypassCache: false,
+        }).then((user) => {
+
+          console.log("User authenticated:", user);
+
+          fetchUserById(user.attributes.sub).then((dataBaseUser) => {
+
+            console.log("Database user:", dataBaseUser);
+            setUserInfo(dataBaseUser);
+
+            fetchCrewsByUser(user.attributes.sub).then((databaseCrews) => {
+
+              console.log("Database crews:", databaseCrews);
+
+              if (databaseCrews) {
+                if (databaseCrews.items) {
+                  const crewItems: Crew[] = databaseCrews.items.filter(
+                    (item) => item !== null
+                  ) as Crew[];
+                  setUserCrews(crewItems);
+                } else {
+                  console.log("Invalid data format for userCrewsData: Missing 'items' field");
+                }
+              } else {
+                console.log("No crews data received or data format is invalid.");
+              }
+
+              console.log("User info fetch successful.");
+
+            });
+          });
         });
-  
-        console.log("User authenticated:", user);
-  
-        const databaseUser = await fetchUserById(user.attributes.sub);
-  
-        console.log("Database user:", databaseUser);
-  
-        const databaseCrews = await fetchCrewsByUser(user.attributes.sub);
-  
-        console.log("Database crews:", databaseCrews);
-  
-        setUserInfo(databaseUser);
-  
-        if (databaseCrews) {
-          if (databaseCrews.items) {
-            const crewItems: Crew[] = databaseCrews.items.filter(
-              (item) => item !== null
-            ) as Crew[];
-            setUserCrews(crewItems);
-          } else {
-            console.log("Invalid data format for userCrewsData: Missing 'items' field");
-          }
-        } else {
-          console.log("No crews data received or data format is invalid.");
-        }
-  
-        console.log("User info fetch successful.");
       } catch (error) {
         console.log("Error during fetchUserInfo:", error);
       }
-    }
+    
   };
-  
+
+
 
   const checkUserRegistrationStatus = async () => {
-    if(isFocused){
-
     console.log("Checking user registration status");
 
-      if (userInfo && userInfo.id) {
+    if (userInfo && userInfo.id) {
 
-        const databaseAttendeeUsers = await getAttendeeUsersByUserID(userInfo.id);
-        if (databaseAttendeeUsers){
-          console.log("Checking user registration status - IN THE 1ST IF ");
-          if(databaseAttendeeUsers.items){
-            console.log("Checking user registration status - IN THE 2ND IF");
+      getAttendeeUsersByUserID(userInfo.id).then((user) => {
 
-            const userItems: AttendeeUser[] = databaseAttendeeUsers.items.filter(
-              (item) => item !== null
-            ) as AttendeeUser[];
-            await setAttendeeUsers(userItems);
-          } else {
-            console.log(
-              "Invalid data format for userCrewsData: Missing 'items' field"
-            );
-          }
+        if (user) {
+          const userItems: AttendeeUser[] = user.items.filter(
+            (item) => item !== null
+          ) as AttendeeUser[];
+          setAttendeeUsers(userItems);
         }
 
         if (attendeeUsers && attendeeUsers.length > 0) {
           console.log("Checking user registration status - IN THE 3RD IF");
           // Check if the current user's ID is in the list of attendees
-          const isRegistered = attendeeUsers.some(user => user.eventId === eventId);  
+          const isRegistered = attendeeUsers.some(user => user.eventId === eventId);
           console.log("Is user registered?: ", isRegistered);
           setIsUserRegistered(isRegistered);
         }
-        else{
+        else {
           setIsUserRegistered(false);
         }
-        
-      }
+
+      });
     }
+
   };
 
   const handleRegisterUser = async () => {
@@ -163,8 +158,8 @@ const SignUpForEventScreen: React.FC<EventScreenProps> = ({ route }) => {
     if (userInfo && userInfo.id) {
 
       const databaseAttendeeUsers = await getAttendeeUsersByUserID(userInfo.id);
-      if (databaseAttendeeUsers){
-        if(databaseAttendeeUsers.items){
+      if (databaseAttendeeUsers) {
+        if (databaseAttendeeUsers.items) {
 
           const userItems: AttendeeUser[] = databaseAttendeeUsers.items.filter(
             (item) => item !== null
@@ -178,7 +173,7 @@ const SignUpForEventScreen: React.FC<EventScreenProps> = ({ route }) => {
       }
       if (attendeeUsers && attendeeUsers.length > 0) {
         // Check if the current user's ID is in the list of attendees
-        const isRegistered = attendeeUsers.some(user => user.eventId === eventId);  
+        const isRegistered = attendeeUsers.some(user => user.eventId === eventId);
         console.log("Is user registered?: ", isRegistered);
 
         if (isRegistered) {
@@ -193,47 +188,43 @@ const SignUpForEventScreen: React.FC<EventScreenProps> = ({ route }) => {
   };
 
   const checkCrewRegistrationStatus = async (crewId: string) => {
-    if(isFocused){
-
     console.log("Checking crew registration status for crewId:", crewId);
 
-  
-      const databaseAttendeeCrew = await getAttendeeCrewsByEventID(eventId);
-  
-      if (databaseAttendeeCrew) {
-        console.log("Checking crew registration status - IN THE 1ND IF");
-        if (databaseAttendeeCrew.items) {
-          console.log("Checking crew registration status - IN THE 2ND IF");
-          const crewItems: AttendeeCrew[] = databaseAttendeeCrew.items.filter(
-            (item) => item !== null
-          ) as AttendeeCrew[];
-          await  setAttendeeCrews(crewItems);
-        } else {
-          console.log(
-            "Invalid data format for userCrewsData: Missing 'items' field"
-          );
-        }
-  
-        if (attendeeCrews && attendeeCrews.length > 0) {
-          console.log("Checking crew registration status - IN THE 3RD IF");
-          // Check if the current crew's ID is in the list of attendees
-          const isCrewRegistered = attendeeCrews.some((crew) => crew.crewId === crewId);
-          console.log("Is crew registered?:  ", isCrewRegistered);
-          setIsCrewRegistered((prevState) => ({
-            ...prevState,
-            [crewId]: isCrewRegistered,
-          }));
-        }else {
-          // If attendeeCrews is falsy or has length 0, set false for all crewIds
-          setIsCrewRegistered((prevState) => ({
-            ...prevState,
-            [crewId]: false,
-          }));
-        }
+
+    getAttendeeCrewsByEventID(eventId).then((crews) => {
+
+      if (crews) {
+        console.log("Checking crew registration status - IN THE 2ND IF");
+        const crewItems: AttendeeCrew[] = crews.items.filter(
+          (item) => item !== null
+        ) as AttendeeCrew[];
+        setAttendeeCrews(crewItems);
+      } else {
+        console.log(
+          "Invalid data format for userCrewsData: Missing 'items' field"
+        );
       }
-    }
+
+      if (attendeeCrews && attendeeCrews.length > 0) {
+        console.log("Checking crew registration status - IN THE 3RD IF");
+        // Check if the current crew's ID is in the list of attendees
+        const isCrewRegistered = attendeeCrews.some((crew) => crew.crewId === crewId);
+        console.log("Is crew registered?:  ", isCrewRegistered);
+        setIsCrewRegistered((prevState) => ({
+          ...prevState,
+          [crewId]: isCrewRegistered,
+        }));
+      } else {
+        // If attendeeCrews is falsy or has length 0, set false for all crewIds
+        setIsCrewRegistered((prevState) => ({
+          ...prevState,
+          [crewId]: false,
+        }));
+      }
+    });
+
   };
-  
+
 
   const handleRegisterCrew = async (crewId: string) => {
     addCrewAttendee(eventId, crewId);
@@ -244,77 +235,110 @@ const SignUpForEventScreen: React.FC<EventScreenProps> = ({ route }) => {
   const handleUnRegisterCrew = async (crewID: string) => {
 
     const databaseAttendeeCrew = await getAttendeeCrewsByEventID(eventId);
-  
-      if (databaseAttendeeCrew) {
-        if (databaseAttendeeCrew.items) {
-          const crewItems: AttendeeCrew[] = databaseAttendeeCrew.items.filter(
-            (item) => item !== null
-          ) as AttendeeCrew[];
-          setAttendeeCrews(crewItems);
-        } else {
-          console.log(
-            "Invalid data format for userCrewsData: Missing 'items' field"
-          );
-        }
-  
-        if (attendeeCrews && attendeeCrews.length > 0) {
-          // Check if the current crew's ID is in the list of attendees
-          const isCrewRegistered = attendeeCrews.some((crew) => crew.crewId === crewID);
-          console.log("Is crew registered?: ", isCrewRegistered);
 
-          if(isCrewRegistered){
-            const attendeeCrewID = attendeeCrews.find((crew) => crew.crewId === crewID)?.id;
-            if(attendeeCrewID){
-              deleteCrewAttendee(attendeeCrewID);
-    setIsCrewRegistered((prevState) => ({ ...prevState, [crewID]: false }));
-    console.log("Deleting crew attendee: " + crewID);
-            }
+    if (databaseAttendeeCrew) {
+      if (databaseAttendeeCrew.items) {
+        const crewItems: AttendeeCrew[] = databaseAttendeeCrew.items.filter(
+          (item) => item !== null
+        ) as AttendeeCrew[];
+        setAttendeeCrews(crewItems);
+      } else {
+        console.log(
+          "Invalid data format for userCrewsData: Missing 'items' field"
+        );
+      }
+
+      if (attendeeCrews && attendeeCrews.length > 0) {
+        // Check if the current crew's ID is in the list of attendees
+        const isCrewRegistered = attendeeCrews.some((crew) => crew.crewId === crewID);
+        console.log("Is crew registered?: ", isCrewRegistered);
+
+        if (isCrewRegistered) {
+          const attendeeCrewID = attendeeCrews.find((crew) => crew.crewId === crewID)?.id;
+          if (attendeeCrewID) {
+            deleteCrewAttendee(attendeeCrewID);
+            setIsCrewRegistered((prevState) => ({ ...prevState, [crewID]: false }));
+            console.log("Deleting crew attendee: " + crewID);
           }
         }
       }
+    }
   };
+
+  const TestMethod = async () => {
+
+    fetchUserInfo().then(() => {
+
+      checkUserRegistrationStatus().then(() => {
+
+        if (userCrews && userCrews.length > 0) {
+          for (const crew of userCrews) {
+            checkCrewRegistrationStatus(crew.id).then(() => {
+
+              console.log("FGHYUJIOFKJRHBE3NWJKOVIRUJH3WNKVOIUJHRBNEWKOPI");
+
+            });
+          }
+        }
+      })
+    })
+  }
+
+
+  const TestMethod2 = async () => {
+
+    TestMethod().then(() => {
+
+      function sleep(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+
+
+      sleep(2000).then(() => {
+        TestMethod();
+      });
+    });
+  }
+
 
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Fetch 
-        console.log("Start fetching user info...");
-        await fetchUserInfo();
 
-        // Proceed with checks only if user info is available
-        if (userInfo) {
-          await checkUserRegistrationStatus();
+    fetchUserInfo().then((response) => {
+      console.log(" THIS IS WHAT I'M LOOKING FOR - AFTER THE FETCH USER INFO");
+      console.log(" THIS IS WHAT I'M LOOKING FOR - USER INFO RESPOSE: ", response);
 
-          if (userCrews && userCrews.length > 0) {
-            // Use a recursive function to iterate over crews one at a time
-            const processCrews = async (index: number) => {
-              if (index < userCrews.length) {
-                await checkCrewRegistrationStatus(userCrews[index].id);
-                await processCrews(index + 1);
-              }
-            };
 
-            // Start the recursive processing
-            await processCrews(0);
+      checkUserRegistrationStatus().then((response) => {
+        console.log(" THIS IS WHAT I'M LOOKING FOR - AFTER THE CHECK USER REGIS");
+        console.log(" THIS IS WHAT I'M LOOKING FOR - CHECK USER REGIS RESPOSE: ", response);
+
+        console.log("THIS IS WHAT I'M LOOKING FOR - ", userCrews?.length);
+
+        if (userCrews && userCrews.length > 0) {
+          for (const crew of userCrews) {
+            checkCrewRegistrationStatus(crew.id).then(() => {
+
+              console.log(" THIS IS WHAT I'M LOOKING FOR - AFTER THE CHECK CREW REGI");
+              setIsLoading(false);
+            });
           }
         }
+      })
+    })
 
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Error during data fetching:", error);
-        // Handle errors as needed
-      }
-    };
-    fetchData();
-  }, [isFocused]);
-
-  // ... (existing code)
+  }, []);
 
 
-  if (isLoading) {
-    return <LoadingSpinner />;
-  }
+  useEffect(() => {
+
+    console.log('Is loading change');
+    console.log(isLoading);
+
+
+
+  }, [isLoading])
+
 
   return (
     <SafeAreaView>
@@ -327,75 +351,81 @@ const SignUpForEventScreen: React.FC<EventScreenProps> = ({ route }) => {
             time={eventTime}
             brief={eventBrief}
           />
-          <View style={SignUpForEventStyles.container}>
-            <View style={SignUpForEventStyles.userImageContainer}>
-              <Image
-                source={require("../../../assets/my-infomration-sceen/lego-figure.png")}
-                style={SignUpForEventStyles.userImage}
-              />
-            </View>
+          {isLoading === false ? (
+            <View style={SignUpForEventStyles.container}>
+              <View style={SignUpForEventStyles.userImageContainer}>
+                <Image
+                  source={require("../../../assets/my-infomration-sceen/lego-figure.png")}
+                  style={SignUpForEventStyles.userImage}
+                />
+                <CustomButton name="TEST" action={() => TestMethod()} />
+              </View>
 
-            <View style={SignUpForEventStyles.userNameContainer}>
-              <Text style={SignUpForEventStyles.infoItem}>
-                {userInfo?.firstname} {userInfo?.lastname}
-              </Text>
-              {isUserRegistered ? (
-                <CustomButton
-                  name={"Unregister"}
-                  action={() => handleUnRegisterUser()}
-                />
+              <View style={SignUpForEventStyles.userNameContainer}>
+                <Text style={SignUpForEventStyles.infoItem}>
+                  {userInfo?.firstname} {userInfo?.lastname}
+                </Text>
+                {isUserRegistered ? (
+                  <CustomButton
+                    name={"Unregister"}
+                    action={() => handleUnRegisterUser()}
+                  />
+                ) : (
+                  <CustomButton
+                    name={"Register"}
+                    action={() => handleRegisterUser()}
+                  />
+                )}
+              </View>
+              <Text style={SignUpForEventStyles.crewsection}>My crew:</Text>
+              {userCrews && userCrews ? (
+                userCrews.length === 0 ? (
+                  <Text style={SignUpForEventStyles.noCrewText}>
+                    Currently you have no crew.
+                  </Text>
+                ) : (
+                  <View style={SignUpForEventStyles.crewGrid}>
+                    {userCrews.map((item) =>
+                      item ? (
+                        <View
+                          style={SignUpForEventStyles.crewCards}
+                          key={item.id}
+                        >
+                          <CrewCard crew={item} />
+
+                          {isCrewRegistered[item.id] ? (
+                            <TouchableOpacity
+                              style={[SignUpForEventStyles.button]}
+                              onPress={() => handleUnRegisterCrew(item.id)}
+                            >
+                              <Text style={SignUpForEventStyles.buttonText}>
+                                Unregister
+                              </Text>
+                            </TouchableOpacity>
+                          ) : (
+                            <TouchableOpacity
+                              style={[SignUpForEventStyles.button]}
+                              onPress={() => handleRegisterCrew(item.id)}
+                            >
+                              <Text style={SignUpForEventStyles.buttonText}>
+                                Register
+                              </Text>
+                            </TouchableOpacity>
+                          )}
+                        </View>
+                      ) : null
+                    )}
+                  </View>
+                )
               ) : (
-                <CustomButton
-                  name={"Register"}
-                  action={() => handleRegisterUser()}
-                />
+                <LoadingSpinner />
               )}
             </View>
-            <Text style={SignUpForEventStyles.crewsection}>My crew:</Text>
-            {userCrews && userCrews ? (
-              userCrews.length === 0 ? (
-                <Text style={SignUpForEventStyles.noCrewText}>
-                  Currently you have no crew.
-                </Text>
-              ) : (
-                <View style={SignUpForEventStyles.crewGrid}>
-                  {userCrews.map((item) =>
-                    item ? (
-                      <View
-                        style={SignUpForEventStyles.crewCards}
-                        key={item.id}
-                      >
-                        <CrewCard crew={item} />
-
-                        {isCrewRegistered[item.id] ? (
-                          <TouchableOpacity
-                            style={[SignUpForEventStyles.button]}
-                            onPress={() => handleUnRegisterCrew(item.id)}
-                          >
-                            <Text style={SignUpForEventStyles.buttonText}>
-                              Unregister
-                            </Text>
-                          </TouchableOpacity>
-                        ) : (
-                          <TouchableOpacity
-                            style={[SignUpForEventStyles.button]}
-                            onPress={() => handleRegisterCrew(item.id)}
-                          >
-                            <Text style={SignUpForEventStyles.buttonText}>
-                              Register
-                            </Text>
-                          </TouchableOpacity>
-                        )}
-                      </View>
-                    ) : null
-                  )}
-                </View>
-              )
-            ) : (
-              <LoadingSpinner />
-            )}
-          </View>
+          ) : (
+            <LoadingSpinner />
+          )}
         </View>
+
       </ScrollView>
     </SafeAreaView>
   );
