307d93f718af0a604574729e36618f6cd1ae7fd4
kyra <kyra.tolnai@lego.com>
2023-11-30 19:08:46 +0100

    Removing attendee crew

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -2,32 +2,9 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-import * as APITypes from "../API";
-type GeneratedQuery<InputType, OutputType> = string & {
-  __generatedQueryInput: InputType;
-  __generatedQueryOutput: OutputType;
-};
-
-export const getUser = /* GraphQL */ `query GetUser($id: ID!) {
-  getUser(id: $id) {
-    id
-    firstname
-    lastname
-    userType
-    email
-    createdAt
-    updatedAt
-    __typename
-  }
-}
-` as GeneratedQuery<APITypes.GetUserQueryVariables, APITypes.GetUserQuery>;
-export const listUsers = /* GraphQL */ `query ListUsers(
-  $filter: ModelUserFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
+export const getUser = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
       id
       firstname
       lastname
@@ -37,32 +14,33 @@ export const listUsers = /* GraphQL */ `query ListUsers(
       updatedAt
       __typename
     }
-    nextToken
-    __typename
   }
-}
-` as GeneratedQuery<APITypes.ListUsersQueryVariables, APITypes.ListUsersQuery>;
-export const getVenue = /* GraphQL */ `query GetVenue($id: ID!) {
-  getVenue(id: $id) {
-    id
-    name
-    shortDescription
-    description
-    capacity
-    openingHours
-    createdAt
-    updatedAt
-    __typename
+`;
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUserFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        firstname
+        lastname
+        userType
+        email
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
   }
-}
-` as GeneratedQuery<APITypes.GetVenueQueryVariables, APITypes.GetVenueQuery>;
-export const listVenues = /* GraphQL */ `query ListVenues(
-  $filter: ModelVenueFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listVenues(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
+`;
+export const getVenue = /* GraphQL */ `
+  query GetVenue($id: ID!) {
+    getVenue(id: $id) {
       id
       name
       shortDescription
@@ -73,36 +51,34 @@ export const listVenues = /* GraphQL */ `query ListVenues(
       updatedAt
       __typename
     }
-    nextToken
-    __typename
   }
-}
-` as GeneratedQuery<
-  APITypes.ListVenuesQueryVariables,
-  APITypes.ListVenuesQuery
->;
-export const getCrew = /* GraphQL */ `query GetCrew($id: ID!) {
-  getCrew(id: $id) {
-    id
-    userId
-    firstname
-    lastname
-    familyRole
-    dateOfBirth
-    email
-    createdAt
-    updatedAt
-    __typename
+`;
+export const listVenues = /* GraphQL */ `
+  query ListVenues(
+    $filter: ModelVenueFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listVenues(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        shortDescription
+        description
+        capacity
+        openingHours
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
   }
-}
-` as GeneratedQuery<APITypes.GetCrewQueryVariables, APITypes.GetCrewQuery>;
-export const listCrews = /* GraphQL */ `query ListCrews(
-  $filter: ModelCrewFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listCrews(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
+`;
+export const getCrew = /* GraphQL */ `
+  query GetCrew($id: ID!) {
+    getCrew(id: $id) {
       id
       userId
       firstname
@@ -114,36 +90,35 @@ export const listCrews = /* GraphQL */ `query ListCrews(
       updatedAt
       __typename
     }
-    nextToken
-    __typename
   }
-}
-` as GeneratedQuery<APITypes.ListCrewsQueryVariables, APITypes.ListCrewsQuery>;
-export const getEvent = /* GraphQL */ `query GetEvent($id: ID!) {
-  getEvent(id: $id) {
-    id
-    title
-    brief
-    description
-    agenda
-    startDateTime
-    endDateTime
-    numOfTickets
-    host
-    venueId
-    createdAt
-    updatedAt
-    __typename
+`;
+export const listCrews = /* GraphQL */ `
+  query ListCrews(
+    $filter: ModelCrewFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listCrews(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        userId
+        firstname
+        lastname
+        familyRole
+        dateOfBirth
+        email
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
   }
-}
-` as GeneratedQuery<APITypes.GetEventQueryVariables, APITypes.GetEventQuery>;
-export const listEvents = /* GraphQL */ `query ListEvents(
-  $filter: ModelEventFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listEvents(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
+`;
+export const getEvent = /* GraphQL */ `
+  query GetEvent($id: ID!) {
+    getEvent(id: $id) {
       id
       title
       brief
@@ -158,35 +133,38 @@ export const listEvents = /* GraphQL */ `query ListEvents(
       updatedAt
       __typename
     }
-    nextToken
-    __typename
   }
-}
-` as GeneratedQuery<
-  APITypes.ListEventsQueryVariables,
-  APITypes.ListEventsQuery
->;
-export const getAttendeeUser = /* GraphQL */ `query GetAttendeeUser($id: ID!) {
-  getAttendeeUser(id: $id) {
-    eventId
-    userId
-    id
-    createdAt
-    updatedAt
-    __typename
+`;
+export const listEvents = /* GraphQL */ `
+  query ListEvents(
+    $filter: ModelEventFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listEvents(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        brief
+        description
+        agenda
+        startDateTime
+        endDateTime
+        numOfTickets
+        host
+        venueId
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
   }
-}
-` as GeneratedQuery<
-  APITypes.GetAttendeeUserQueryVariables,
-  APITypes.GetAttendeeUserQuery
->;
-export const listAttendeeUsers = /* GraphQL */ `query ListAttendeeUsers(
-  $filter: ModelAttendeeUserFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listAttendeeUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
+`;
+export const getAttendeeUser = /* GraphQL */ `
+  query GetAttendeeUser($id: ID!) {
+    getAttendeeUser(id: $id) {
       eventId
       userId
       id
@@ -194,208 +172,148 @@ export const listAttendeeUsers = /* GraphQL */ `query ListAttendeeUsers(
       updatedAt
       __typename
     }
-    nextToken
-    __typename
   }
-}
-` as GeneratedQuery<
-  APITypes.ListAttendeeUsersQueryVariables,
-  APITypes.ListAttendeeUsersQuery
->;
-export const getAttendeeCrew = /* GraphQL */ `query GetAttendeeCrew($id: ID!) {
-  getAttendeeCrew(id: $id) {
-    eventId
-    crewId
-    id
-    createdAt
-    updatedAt
-    __typename
-  }
-}
-` as GeneratedQuery<
-  APITypes.GetAttendeeCrewQueryVariables,
-  APITypes.GetAttendeeCrewQuery
->;
-export const listAttendeeCrews = /* GraphQL */ `query ListAttendeeCrews(
-  $filter: ModelAttendeeCrewFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listAttendeeCrews(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      eventId
-      crewId
-      id
-      createdAt
-      updatedAt
-      __typename
-    }
-    nextToken
-    __typename
-  }
-}
-` as GeneratedQuery<
-  APITypes.ListAttendeeCrewsQueryVariables,
-  APITypes.ListAttendeeCrewsQuery
->;
-export const crewsByUserId = /* GraphQL */ `query CrewsByUserId(
-  $userId: String!
-  $sortDirection: ModelSortDirection
-  $filter: ModelCrewFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  crewsByUserId(
-    userId: $userId
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
+`;
+export const listAttendeeUsers = /* GraphQL */ `
+  query ListAttendeeUsers(
+    $filter: ModelAttendeeUserFilterInput
+    $limit: Int
+    $nextToken: String
   ) {
-    items {
-      id
-      userId
-      firstname
-      lastname
-      familyRole
-      dateOfBirth
-      email
-      createdAt
-      updatedAt
+    listAttendeeUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        eventId
+        userId
+        id
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
       __typename
     }
-    nextToken
-    __typename
   }
-}
-` as GeneratedQuery<
-  APITypes.CrewsByUserIdQueryVariables,
-  APITypes.CrewsByUserIdQuery
->;
-export const eventsByVenueId = /* GraphQL */ `query EventsByVenueId(
-  $venueId: String!
-  $sortDirection: ModelSortDirection
-  $filter: ModelEventFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  eventsByVenueId(
-    venueId: $venueId
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
+`;
+export const crewsByUserId = /* GraphQL */ `
+  query CrewsByUserId(
+    $userId: String!
+    $sortDirection: ModelSortDirection
+    $filter: ModelCrewFilterInput
+    $limit: Int
+    $nextToken: String
   ) {
-    items {
-      id
-      title
-      brief
-      description
-      agenda
-      startDateTime
-      endDateTime
-      numOfTickets
-      host
-      venueId
-      createdAt
-      updatedAt
+    crewsByUserId(
+      userId: $userId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        userId
+        firstname
+        lastname
+        familyRole
+        dateOfBirth
+        email
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
       __typename
     }
-    nextToken
-    __typename
   }
-}
-` as GeneratedQuery<
-  APITypes.EventsByVenueIdQueryVariables,
-  APITypes.EventsByVenueIdQuery
->;
-export const attendeeUsersByEventId = /* GraphQL */ `query AttendeeUsersByEventId(
-  $eventId: String!
-  $sortDirection: ModelSortDirection
-  $filter: ModelAttendeeUserFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  attendeeUsersByEventId(
-    eventId: $eventId
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
+`;
+export const eventsByVenueId = /* GraphQL */ `
+  query EventsByVenueId(
+    $venueId: String!
+    $sortDirection: ModelSortDirection
+    $filter: ModelEventFilterInput
+    $limit: Int
+    $nextToken: String
   ) {
-    items {
-      eventId
-      userId
-      id
-      createdAt
-      updatedAt
+    eventsByVenueId(
+      venueId: $venueId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        title
+        brief
+        description
+        agenda
+        startDateTime
+        endDateTime
+        numOfTickets
+        host
+        venueId
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
       __typename
     }
-    nextToken
-    __typename
   }
-}
-` as GeneratedQuery<
-  APITypes.AttendeeUsersByEventIdQueryVariables,
-  APITypes.AttendeeUsersByEventIdQuery
->;
-export const attendeeUsersByUserId = /* GraphQL */ `query AttendeeUsersByUserId(
-  $userId: String!
-  $sortDirection: ModelSortDirection
-  $filter: ModelAttendeeUserFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  attendeeUsersByUserId(
-    userId: $userId
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
+`;
+export const attendeeUsersByEventId = /* GraphQL */ `
+  query AttendeeUsersByEventId(
+    $eventId: String!
+    $sortDirection: ModelSortDirection
+    $filter: ModelAttendeeUserFilterInput
+    $limit: Int
+    $nextToken: String
   ) {
-    items {
-      eventId
-      userId
-      id
-      createdAt
-      updatedAt
+    attendeeUsersByEventId(
+      eventId: $eventId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        eventId
+        userId
+        id
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
       __typename
     }
-    nextToken
-    __typename
   }
-}
-` as GeneratedQuery<
-  APITypes.AttendeeUsersByUserIdQueryVariables,
-  APITypes.AttendeeUsersByUserIdQuery
->;
-export const attendeeCrewsByEventId = /* GraphQL */ `query AttendeeCrewsByEventId(
-  $eventId: String!
-  $sortDirection: ModelSortDirection
-  $filter: ModelAttendeeCrewFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  attendeeCrewsByEventId(
-    eventId: $eventId
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
+`;
+export const attendeeUsersByUserId = /* GraphQL */ `
+  query AttendeeUsersByUserId(
+    $userId: String!
+    $sortDirection: ModelSortDirection
+    $filter: ModelAttendeeUserFilterInput
+    $limit: Int
+    $nextToken: String
   ) {
-    items {
-      eventId
-      crewId
-      id
-      createdAt
-      updatedAt
+    attendeeUsersByUserId(
+      userId: $userId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        eventId
+        userId
+        id
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
       __typename
     }
-    nextToken
-    __typename
   }
-}
-` as GeneratedQuery<
-  APITypes.AttendeeCrewsByEventIdQueryVariables,
-  APITypes.AttendeeCrewsByEventIdQuery
->;
+`;
--- src/graphql/subscriptions.ts
+++ src/graphql/subscriptions.ts
@@ -2,321 +2,237 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-import * as APITypes from "../API";
-type GeneratedSubscription<InputType, OutputType> = string & {
-  __generatedSubscriptionInput: InputType;
-  __generatedSubscriptionOutput: OutputType;
-};
-
-export const onCreateUser = /* GraphQL */ `subscription OnCreateUser($filter: ModelSubscriptionUserFilterInput) {
-  onCreateUser(filter: $filter) {
-    id
-    firstname
-    lastname
-    userType
-    email
-    createdAt
-    updatedAt
-    __typename
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser($filter: ModelSubscriptionUserFilterInput) {
+    onCreateUser(filter: $filter) {
+      id
+      firstname
+      lastname
+      userType
+      email
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnCreateUserSubscriptionVariables,
-  APITypes.OnCreateUserSubscription
->;
-export const onUpdateUser = /* GraphQL */ `subscription OnUpdateUser($filter: ModelSubscriptionUserFilterInput) {
-  onUpdateUser(filter: $filter) {
-    id
-    firstname
-    lastname
-    userType
-    email
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser($filter: ModelSubscriptionUserFilterInput) {
+    onUpdateUser(filter: $filter) {
+      id
+      firstname
+      lastname
+      userType
+      email
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnUpdateUserSubscriptionVariables,
-  APITypes.OnUpdateUserSubscription
->;
-export const onDeleteUser = /* GraphQL */ `subscription OnDeleteUser($filter: ModelSubscriptionUserFilterInput) {
-  onDeleteUser(filter: $filter) {
-    id
-    firstname
-    lastname
-    userType
-    email
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser($filter: ModelSubscriptionUserFilterInput) {
+    onDeleteUser(filter: $filter) {
+      id
+      firstname
+      lastname
+      userType
+      email
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnDeleteUserSubscriptionVariables,
-  APITypes.OnDeleteUserSubscription
->;
-export const onCreateVenue = /* GraphQL */ `subscription OnCreateVenue($filter: ModelSubscriptionVenueFilterInput) {
-  onCreateVenue(filter: $filter) {
-    id
-    name
-    shortDescription
-    description
-    capacity
-    openingHours
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onCreateVenue = /* GraphQL */ `
+  subscription OnCreateVenue($filter: ModelSubscriptionVenueFilterInput) {
+    onCreateVenue(filter: $filter) {
+      id
+      name
+      shortDescription
+      description
+      capacity
+      openingHours
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnCreateVenueSubscriptionVariables,
-  APITypes.OnCreateVenueSubscription
->;
-export const onUpdateVenue = /* GraphQL */ `subscription OnUpdateVenue($filter: ModelSubscriptionVenueFilterInput) {
-  onUpdateVenue(filter: $filter) {
-    id
-    name
-    shortDescription
-    description
-    capacity
-    openingHours
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onUpdateVenue = /* GraphQL */ `
+  subscription OnUpdateVenue($filter: ModelSubscriptionVenueFilterInput) {
+    onUpdateVenue(filter: $filter) {
+      id
+      name
+      shortDescription
+      description
+      capacity
+      openingHours
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnUpdateVenueSubscriptionVariables,
-  APITypes.OnUpdateVenueSubscription
->;
-export const onDeleteVenue = /* GraphQL */ `subscription OnDeleteVenue($filter: ModelSubscriptionVenueFilterInput) {
-  onDeleteVenue(filter: $filter) {
-    id
-    name
-    shortDescription
-    description
-    capacity
-    openingHours
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onDeleteVenue = /* GraphQL */ `
+  subscription OnDeleteVenue($filter: ModelSubscriptionVenueFilterInput) {
+    onDeleteVenue(filter: $filter) {
+      id
+      name
+      shortDescription
+      description
+      capacity
+      openingHours
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnDeleteVenueSubscriptionVariables,
-  APITypes.OnDeleteVenueSubscription
->;
-export const onCreateCrew = /* GraphQL */ `subscription OnCreateCrew($filter: ModelSubscriptionCrewFilterInput) {
-  onCreateCrew(filter: $filter) {
-    id
-    userId
-    firstname
-    lastname
-    familyRole
-    dateOfBirth
-    email
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onCreateCrew = /* GraphQL */ `
+  subscription OnCreateCrew($filter: ModelSubscriptionCrewFilterInput) {
+    onCreateCrew(filter: $filter) {
+      id
+      userId
+      firstname
+      lastname
+      familyRole
+      dateOfBirth
+      email
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnCreateCrewSubscriptionVariables,
-  APITypes.OnCreateCrewSubscription
->;
-export const onUpdateCrew = /* GraphQL */ `subscription OnUpdateCrew($filter: ModelSubscriptionCrewFilterInput) {
-  onUpdateCrew(filter: $filter) {
-    id
-    userId
-    firstname
-    lastname
-    familyRole
-    dateOfBirth
-    email
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onUpdateCrew = /* GraphQL */ `
+  subscription OnUpdateCrew($filter: ModelSubscriptionCrewFilterInput) {
+    onUpdateCrew(filter: $filter) {
+      id
+      userId
+      firstname
+      lastname
+      familyRole
+      dateOfBirth
+      email
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnUpdateCrewSubscriptionVariables,
-  APITypes.OnUpdateCrewSubscription
->;
-export const onDeleteCrew = /* GraphQL */ `subscription OnDeleteCrew($filter: ModelSubscriptionCrewFilterInput) {
-  onDeleteCrew(filter: $filter) {
-    id
-    userId
-    firstname
-    lastname
-    familyRole
-    dateOfBirth
-    email
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onDeleteCrew = /* GraphQL */ `
+  subscription OnDeleteCrew($filter: ModelSubscriptionCrewFilterInput) {
+    onDeleteCrew(filter: $filter) {
+      id
+      userId
+      firstname
+      lastname
+      familyRole
+      dateOfBirth
+      email
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnDeleteCrewSubscriptionVariables,
-  APITypes.OnDeleteCrewSubscription
->;
-export const onCreateEvent = /* GraphQL */ `subscription OnCreateEvent($filter: ModelSubscriptionEventFilterInput) {
-  onCreateEvent(filter: $filter) {
-    id
-    title
-    brief
-    description
-    agenda
-    startDateTime
-    endDateTime
-    numOfTickets
-    host
-    venueId
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onCreateEvent = /* GraphQL */ `
+  subscription OnCreateEvent($filter: ModelSubscriptionEventFilterInput) {
+    onCreateEvent(filter: $filter) {
+      id
+      title
+      brief
+      description
+      agenda
+      startDateTime
+      endDateTime
+      numOfTickets
+      host
+      venueId
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnCreateEventSubscriptionVariables,
-  APITypes.OnCreateEventSubscription
->;
-export const onUpdateEvent = /* GraphQL */ `subscription OnUpdateEvent($filter: ModelSubscriptionEventFilterInput) {
-  onUpdateEvent(filter: $filter) {
-    id
-    title
-    brief
-    description
-    agenda
-    startDateTime
-    endDateTime
-    numOfTickets
-    host
-    venueId
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onUpdateEvent = /* GraphQL */ `
+  subscription OnUpdateEvent($filter: ModelSubscriptionEventFilterInput) {
+    onUpdateEvent(filter: $filter) {
+      id
+      title
+      brief
+      description
+      agenda
+      startDateTime
+      endDateTime
+      numOfTickets
+      host
+      venueId
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnUpdateEventSubscriptionVariables,
-  APITypes.OnUpdateEventSubscription
->;
-export const onDeleteEvent = /* GraphQL */ `subscription OnDeleteEvent($filter: ModelSubscriptionEventFilterInput) {
-  onDeleteEvent(filter: $filter) {
-    id
-    title
-    brief
-    description
-    agenda
-    startDateTime
-    endDateTime
-    numOfTickets
-    host
-    venueId
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onDeleteEvent = /* GraphQL */ `
+  subscription OnDeleteEvent($filter: ModelSubscriptionEventFilterInput) {
+    onDeleteEvent(filter: $filter) {
+      id
+      title
+      brief
+      description
+      agenda
+      startDateTime
+      endDateTime
+      numOfTickets
+      host
+      venueId
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnDeleteEventSubscriptionVariables,
-  APITypes.OnDeleteEventSubscription
->;
-export const onCreateAttendeeUser = /* GraphQL */ `subscription OnCreateAttendeeUser(
-  $filter: ModelSubscriptionAttendeeUserFilterInput
-) {
-  onCreateAttendeeUser(filter: $filter) {
-    eventId
-    userId
-    id
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onCreateAttendeeUser = /* GraphQL */ `
+  subscription OnCreateAttendeeUser(
+    $filter: ModelSubscriptionAttendeeUserFilterInput
+  ) {
+    onCreateAttendeeUser(filter: $filter) {
+      eventId
+      userId
+      id
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnCreateAttendeeUserSubscriptionVariables,
-  APITypes.OnCreateAttendeeUserSubscription
->;
-export const onUpdateAttendeeUser = /* GraphQL */ `subscription OnUpdateAttendeeUser(
-  $filter: ModelSubscriptionAttendeeUserFilterInput
-) {
-  onUpdateAttendeeUser(filter: $filter) {
-    eventId
-    userId
-    id
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onUpdateAttendeeUser = /* GraphQL */ `
+  subscription OnUpdateAttendeeUser(
+    $filter: ModelSubscriptionAttendeeUserFilterInput
+  ) {
+    onUpdateAttendeeUser(filter: $filter) {
+      eventId
+      userId
+      id
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnUpdateAttendeeUserSubscriptionVariables,
-  APITypes.OnUpdateAttendeeUserSubscription
->;
-export const onDeleteAttendeeUser = /* GraphQL */ `subscription OnDeleteAttendeeUser(
-  $filter: ModelSubscriptionAttendeeUserFilterInput
-) {
-  onDeleteAttendeeUser(filter: $filter) {
-    eventId
-    userId
-    id
-    createdAt
-    updatedAt
-    __typename
+`;
+export const onDeleteAttendeeUser = /* GraphQL */ `
+  subscription OnDeleteAttendeeUser(
+    $filter: ModelSubscriptionAttendeeUserFilterInput
+  ) {
+    onDeleteAttendeeUser(filter: $filter) {
+      eventId
+      userId
+      id
+      createdAt
+      updatedAt
+      __typename
+    }
   }
-}
-` as GeneratedSubscription<
-  APITypes.OnDeleteAttendeeUserSubscriptionVariables,
-  APITypes.OnDeleteAttendeeUserSubscription
->;
-export const onCreateAttendeeCrew = /* GraphQL */ `subscription OnCreateAttendeeCrew(
-  $filter: ModelSubscriptionAttendeeCrewFilterInput
-) {
-  onCreateAttendeeCrew(filter: $filter) {
-    eventId
-    crewId
-    id
-    createdAt
-    updatedAt
-    __typename
-  }
-}
-` as GeneratedSubscription<
-  APITypes.OnCreateAttendeeCrewSubscriptionVariables,
-  APITypes.OnCreateAttendeeCrewSubscription
->;
-export const onUpdateAttendeeCrew = /* GraphQL */ `subscription OnUpdateAttendeeCrew(
-  $filter: ModelSubscriptionAttendeeCrewFilterInput
-) {
-  onUpdateAttendeeCrew(filter: $filter) {
-    eventId
-    crewId
-    id
-    createdAt
-    updatedAt
-    __typename
-  }
-}
-` as GeneratedSubscription<
-  APITypes.OnUpdateAttendeeCrewSubscriptionVariables,
-  APITypes.OnUpdateAttendeeCrewSubscription
->;
-export const onDeleteAttendeeCrew = /* GraphQL */ `subscription OnDeleteAttendeeCrew(
-  $filter: ModelSubscriptionAttendeeCrewFilterInput
-) {
-  onDeleteAttendeeCrew(filter: $filter) {
-    eventId
-    crewId
-    id
-    createdAt
-    updatedAt
-    __typename
-  }
-}
-` as GeneratedSubscription<
-  APITypes.OnDeleteAttendeeCrewSubscriptionVariables,
-  APITypes.OnDeleteAttendeeCrewSubscription
->;
+`;
